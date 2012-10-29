b1ce2ac1a627ff4f615dfbd5000e3a121db5bde7
amccann <amccann@amccann-PC>
2012-10-29 15:16:34 -0600

    Minor changes to database/task managers. Still need alot of work.

--- src/com/example/cmput301/Task.java
+++ src/com/example/cmput301/Task.java
@@ -4,100 +4,108 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Task
-{
-        public static final int OWNER_LOCAL = 1;
-        public static final int OWNER_REMOTE = 2;
-	private List<Response> responses;
-	
-	private String name;
-	private String description;
-	private String id;
-	private Date timestamp;
-	private Class<Response> type;
-        private int owner;
-	
-	public Task(String name, String description)
-	{
-		this.name = name;
-		this.description = description;
-		responses = new ArrayList<Response>();
-                this.owner = Task.OWNER_LOCAL;
-	}
-	
-	public Task(String name, String description, String id)
-	{
-		this.name = name;
-		this.description = description;
-		this.id = id;
-		responses = new ArrayList<Response>();
-                this.owner = Task.OWNER_LOCAL;
-	}
+/**
+ *
+ * @author amccann
+ */
+public class Task implements Comparable {
+
+    public static final int STATUS_PRIVATE = 1;
+    public static final int STATUS_SHARED = 2;
+    private List<Response> responses;
+    private String name;
+    private String description;
+    private String id;
+    private Date timestamp;
+    private Class<Response> type;
+    private int status;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        responses = new ArrayList<Response>();
+        this.status = Task.STATUS_PRIVATE;
+    }
+
+    public Task(String name, String description, String id) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        responses = new ArrayList<Response>();
+        this.status = Task.STATUS_PRIVATE;
+    }
+
+    public Task(String name, String description, int status) {
+        this.name = name;
+        this.description = description;
+        responses = new ArrayList<Response>();
+        this.status = status;
+    }
+
+    public Task(String name, String description, String id, int status) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        responses = new ArrayList<Response>();
+        this.status = status;
+    }
+
+    public void addResponse(Response response) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void Update() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    // getters and setters
+    public List<Response> getResponses() {
+        return this.responses;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Class<Response> getType() {
+        return this.type;
+    }
+
+    public Date getTimestamp() {
+        return this.timestamp;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public int compareTo(Object another) {
+        if (!(another instanceof Task)) {
+            return -1;
+        } 
         
-        public Task(String name, String description, int owner)
-	{
-		this.name = name;
-		this.description = description;
-		responses = new ArrayList<Response>();
-                this.owner = owner;
-	}
-	
-	public Task(String name, String description, String id, int owner)
-	{
-		this.name = name;
-		this.description = description;
-		this.id = id;
-		responses = new ArrayList<Response>();
-                this.owner = owner;
-	}
+        //If it was an instance of task then make it so.
+         Task anotherTask = (Task) another;
         
+         
+         //Return the string comparison of their ids.
+         return anotherTask.getId().compareTo(this.getId());
         
-	public void addResponse(Response response)
-	{
-		throw new UnsupportedOperationException("Not implemented yet");
-	}
-	
-	public void Update()
-	{
-		throw new UnsupportedOperationException("Not implemented yet");
-	}
-	
-	// getters and setters
-	public List<Response> getResponses()
-	{
-		return this.responses;
-	}
-	public void setId(String id)
-	{
-		this.id = id;
-	}	
-        public void setOwner(int owner) 
-        {
-            this.owner = owner;
-        }
-	public Class<Response> getType()
-	{
-		return this.type;
-	}
-	public Date getTimestamp()
-	{
-		return this.timestamp;
-	}
-	public String getId()
-	{
-		return this.id;
-	}
-	public String getName()
-	{
-		return this.name;
-	}	
-	public String getDescription()
-	{
-		return this.description;
-	}
-        
-        public int getOwner() 
-        {
-            return this.owner;
-        }
+    }
 }
