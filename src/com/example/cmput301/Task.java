f828fa141531e4a3e992d295c8dd86bfe83f3c9e
amccann <amccann@amccann-PC>
2012-11-03 18:44:15 -0600

    Changed Task Class and added new Test Project.

--- src/com/example/cmput301/Task.java
+++ src/com/example/cmput301/Task.java
@@ -10,85 +10,63 @@ import java.util.List;
  * @author amccann
  */
 public class Task implements Comparable, Serializable {
-    
+
     //All remote tasks MUST have a shared status.
     public static final int STATUS_PRIVATE = 1;
     public static final int STATUS_SHARED = 2;
-    
     private List<Response> responses;
     private String name;
     private String description;
     private String id;
-    private Date timestamp;
     private String type;
     private int status;
 
+    /**
+     * Constructor for a new task object with the following properties.
+     * Type is automatically set to TextResponse type.
+     * 
+     * @param name
+     * @param description
+     * @param id
+     * @param status
+     * @param responses 
+     */
+    public Task(String name, String description, String id, int status,
+            List<Response> responses) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.status = status;
+        this.responses = responses;
+        this.type = TextResponse.class.toString();
+    }
+
+    /**
+     * Constructor for a new task object with the following properties. Id is 
+     * set to null, status is Task.STATUS_PRIVATE, type is TextResponse, and 
+     * responses is an empty list.
+     * 
+     * @param name
+     * @param description 
+     */
     public Task(String name, String description) {
         this.name = name;
         this.description = description;
         responses = new ArrayList<Response>();
         this.status = Task.STATUS_PRIVATE;
-		this.type = TextResponse.class.toString();
+        this.type = TextResponse.class.toString();
     }
 
-    /* CAN BE DELTED IMO */
-//    public Task(String name, String description, String id) {
-//        this.name = name;
-//        this.description = description;
-//        this.id = id;
-//        responses = new ArrayList<Response>();
-//        this.status = Task.STATUS_PRIVATE;
-//		this.type = TextResponse.class.toString();
-//    }
-
-//    public Task(String name, String description, int status) {
-//        this.name = name;
-//        this.description = description;
-//        responses = new ArrayList<Response>();
-//        this.status = status;
-//		this.type = TextResponse.class.toString();
-//    }
-//
-//    public Task(String name, String description, String id, int status) {
-//        this.name = name;
-//        this.description = description;
-//        this.id = id;
-//        responses = new ArrayList<Response>();
-//        this.status = status;
-//		this.type = TextResponse.class.toString();
-//    }
-
-    public Task(String name, String description, String id, int status,
-			List<Response> responses)
-	{
-		this.name = name;
-		this.description = description;
-		this.id = id;
-		this.status = status;
-		this.responses = responses;
-		this.type = TextResponse.class.toString();
-	}
-
-	public Task(String name, String description, String id, String type)
-	{
-		this.name = name;
-		this.description = description;
-		this.id = id;
-		this.type = type;
-		this.responses = new ArrayList<Response>();
-		this.type = TextResponse.class.toString();
-	}
-
-	public void addResponse(Response response) {
+    /**
+     * Add a new response to the list of responses.
+     * 
+     * @param response 
+     */
+    public void addResponse(Response response) {
         this.responses.add(response);
     }
 
-
     // getters and setters
-    public List<Response> getResponses() {
-        return this.responses;
-    }
-
     public void setId(String id) {
         this.id = id;
     }
@@ -99,10 +77,6 @@ public class Task implements Comparable, Serializable {
 
     public String getType() {
         return this.type;
-    }
-
-    public Date getTimestamp() {
-        return this.timestamp;
     }
 
     public String getId() {
@@ -120,20 +94,22 @@ public class Task implements Comparable, Serializable {
     public int getStatus() {
         return this.status;
     }
-
+    
+    public List<Response> getResponses() {
+        return this.responses;
+    }
+    
     public int compareTo(Object another) {
         if (!(another instanceof Task)) {
             return -1;
-        } 
-        
+        }
+
         //If it was an instance of task then make it so.
-         Task anotherTask = (Task) another;
-        
-         
-         //Return the string comparison of their ids.
-         return anotherTask.getId().compareTo(this.getId());
-        
+        Task anotherTask = (Task) another;
+
+
+        //Return the string comparison of their ids.
+        return anotherTask.getId().compareTo(this.getId());
+
     }
-
-
 }
