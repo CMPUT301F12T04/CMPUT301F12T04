aecfe8fa3a9218ea0f4c9a8729116f84c4b2462d
davyu <dyu2@ualberta.ca>
2012-11-05 00:06:36 -0700

    documentation added to textresponseview

--- src/com/example/cmput301/TextResponseView.java
+++ src/com/example/cmput301/TextResponseView.java
@@ -14,78 +14,93 @@ import android.widget.EditText;
 import android.widget.ListView;
 import java.util.Date;
 
+/**
+ * This class is a type of response view, it is responsible for getting
+ * input from the user in the form of text and adds it as a response
+ * to the selected task. A view to show the already existing responses
+ * is also available.
+ *
+ */
 @TargetApi(15)
 public class TextResponseView extends ResponseView {
 
-    private MainController mainController;
-    Task t;
-    String responseString;
-    Activity that;
+	private MainController mainController;
+	Task t;
+	String responseString;
+	Activity that;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        that = this;
+	/**
+	 * Overrided method that sets the view of the activity.
+	 * The action bar has a back button enabled, and the title
+	 * of the activity is set to the task title. A list of
+	 * responses for that task is also displayed, with a 
+	 * prompt at the bottom for user text response input to
+	 * that task.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		that = this;
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.text_response_view);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.text_response_view);
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 
-        mainController = new MainController(this.getApplicationContext(), this);
-        String taskTile;
-        
-        //Getting information from bundle passed from MainActivity
-        Bundle bundle = getIntent().getExtras();
-        t = (Task) bundle.getSerializable("task");
-        taskTile = t.getName();
+		mainController = new MainController(this.getApplicationContext(), this);
+		String taskTile;
 
-        //setting the title of the task to be the activity title
-        setTitle(taskTile);
+		//Getting information from bundle passed from MainActivity
+		Bundle bundle = getIntent().getExtras();
+		t = (Task) bundle.getSerializable("task");
+		taskTile = t.getName();
 
+		//setting the title of the task to be the activity title
+		setTitle(taskTile);
 
-
-        //
-        final EditText responseInput;
-        responseInput = (EditText) findViewById(R.id.textResponseInput);
+		final EditText responseInput;
+		responseInput = (EditText) findViewById(R.id.textResponseInput);
 
 
-        Button postBut;
-        postBut = (Button) findViewById(R.id.postResponseButton);
+		Button postBut;
+		postBut = (Button) findViewById(R.id.postResponseButton);
 
 
-        postBut.setOnClickListener(new View.OnClickListener() {
-            //Post has been clicked.
-            public void onClick(View v) {
+		postBut.setOnClickListener(new View.OnClickListener() {
+			//Post has been clicked.
+			public void onClick(View v) {
 
-                //Can now add response to the task using this string
-                responseString = responseInput.getText().toString();
-                TextResponse resp = new TextResponse(responseString, new Date());
-                mainController.addResponse(t, resp);
+				//Can now add response to the task using this string
+				responseString = responseInput.getText().toString();
+				TextResponse resp = new TextResponse(responseString, new Date());
+				mainController.addResponse(t, resp);
 
-                //setting list of responses for task
-                ListView responses = (ListView) findViewById(R.id.text_response_list);
-                @SuppressWarnings({ "rawtypes", "unchecked" })
+				//setting list of responses for task
+				ListView responses = (ListView) findViewById(R.id.text_response_list);
+				@SuppressWarnings({ "rawtypes", "unchecked" })
 				ArrayAdapter<TextResponse> adapter = new ArrayAdapter<TextResponse>(that,
-                        android.R.layout.simple_list_item_1, (ArrayList) t.getResponses());
-                responses.setAdapter(adapter);
-            }
-        });
+						android.R.layout.simple_list_item_1, (ArrayList) t.getResponses());
+				responses.setAdapter(adapter);
+			}
+		});
 
-        //setting list of responses for task
-        ListView responses = (ListView) findViewById(R.id.text_response_list);
-        @SuppressWarnings({ "rawtypes", "unchecked" })
+		//setting list of responses for task
+		ListView responses = (ListView) findViewById(R.id.text_response_list);
+		@SuppressWarnings({ "rawtypes", "unchecked" })
 		ArrayAdapter<TextResponse> adapter = new ArrayAdapter<TextResponse>(this,
-                android.R.layout.simple_list_item_1, (ArrayList) t.getResponses());
-        responses.setAdapter(adapter);
-    }
+				android.R.layout.simple_list_item_1, (ArrayList) t.getResponses());
+		responses.setAdapter(adapter);
+	}
+	
+    /**
+     * Overrided method that just enables the back button to kill the activity
+     */
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        //go back, kills activity
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
-        return true;
-    }
+		//go back, kills activity
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+		}
+		return true;
+	}
 }
