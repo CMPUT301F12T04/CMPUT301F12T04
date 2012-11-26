b2c2382ca71a90e548dfb429e98086b2bdd8f6bd
Jason Reddekopp <jreddeko@ualberta.ca>
2012-11-26 12:32:36 -0700

    DatabaseSingleton.java : global singleton to stop database from opening closing contantly

---
+++ src/com/example/cmput301/DatabaseSingleton.java
@@ -0,0 +1,34 @@
+package com.example.cmput301;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatabaseSingleton
+{
+	private static volatile DatabaseSingleton instance = null;
+	
+	public static DatabaseSingleton getInstance(Context context)
+	{
+		if(instance==null)
+		{
+			synchronized(DatabaseSingleton.class)
+			{
+				if(instance==null)
+				{
+					instance = new DatabaseSingleton(context.getApplicationContext());
+				}
+			}
+		}
+		return instance;
+	}
+	private SQLiteDatabase db;
+	public DatabaseSingleton(Context context)
+	{
+		db = new DatabaseOpenHelper(context).getWritableDatabase();
+	}
+	public SQLiteDatabase getDB()
+	{
+		return this.db;
+	}
+
+}
